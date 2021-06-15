611329006ef1ca3374a6838a5a722fcdbd2e46f3
lauris79 <latpages@gmail.com>
2021-06-15 08:44:45 +0300

    Added Yii2 BackupController

---
+++ controllers/yii2/BackupController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace DbBackup\controllers\yii2;
+
+use alpha\voyage\Module;
+use DbBackup\Dumper;
+use DbBackup\Sync;
+use yii\console\Controller;
+use yii\console\ExitCode;
+
+#### WARNING! Requires Yii2 installed! ####
+
+/**
+* Class BackupController* @property Module $module
+*/
+class BackupController extends Controller
+{       
+    public $interval;
+    
+    public function options($actionID)
+    {
+        return ['interval'];
+    }
+    
+    /**
+     * @return string[]
+     */
+    public function optionAliases()
+    {
+        return ['i' => 'interval'];
+    }
+    
+    /**
+     * default action
+     * @return int
+     * @throws \Exception
+     */
+    public function actionDump(): int
+    {
+        $dumper = new Dumper(['interval' => $this->interval, 'cronPath' => $this->getCronPath('dump')]);
+        $dumper->setAuthFromEnv();
+        echo $dumper->run();
+        
+        return ExitCode::OK;
+    }
+    
+    /**
+     * default action
+     * @return int
+     * @throws \Exception
+     */
+    public function actionSync(): int
+    {
+        $sync = new Sync(['interval' => $this->interval, 'cronPath' => $this->getCronPath('sync')]);
+        $sync->setAuthFromEnv();
+        echo $sync->run();
+        
+        return ExitCode::OK;
+    }
+    
+    public function getCronPath(string $action)
+    {
+        $rootPath = realpath(\Yii::getAlias('@vendor') . '/../');
+        $cronPath = $rootPath . '/yii backup/' . $action . ' -i=' . $this->interval;
+
+        return$cronPath;
+    }
+}
